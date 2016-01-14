8dcdbfab619de676f51793c4125e2625aa1628e6
berger19 <berger27@naver.com>
2016-01-14 10:27:51 +0900

    conflict solution

--- MultiNetworkGame/src/HelloGitHub.java
+++ MultiNetworkGame/src/HelloGitHub.java
@@ -4,7 +4,7 @@ public class HelloGitHub {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello Git!");
-		System.out.println("Git Large File Storage(LFS)")
+		System.out.println("Git Large File Storage(LFS)");
 	}
 
 }
